1d0cc2820d846bb38e48800edc27f885aa879c32
Moathal <moathal.s.kachi@gmail.com>
2022-09-17 09:15:04 +0300

    Install stylelint

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Book({ title, author, button }) {
   return (
-    <>
-      <li>{title}</li>
-      <li>{author}</li>
+    <li>
+      <p>{title}</p>
+      <p>{author}</p>
       <button type="button">{button}</button>
-    </>
+    </li>
   );
 }
