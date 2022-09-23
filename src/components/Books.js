300a8f21d5ace92ce31dbc482bb1f28449cc9dc4
Moathal <moathal.s.kachi@gmail.com>
2022-09-23 06:55:40 +0300

    Call the store and loop over it to show books
    Call the store and loop it to show status on click
    
    Add functionnality to add a book
    
    Add functionality to remove a book

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 export default function Books() {
+  const books = useSelector((state) => state.updateList);
   return (
     <>
       <ul>
-        <Book title="title" author="author" button="Remove" />
+        {books.map((book) => (
+          <Book
+            title={book.title}
+            author={book.author}
+            key={book.id}
+            button="Remove"
+            id={book.id}
+          />
+        ))}
       </ul>
       <Form />
     </>
